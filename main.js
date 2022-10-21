b2741e070bb953d451309e60ccf1017a4d345297
kadayesha <93032523+kadayesha@users.noreply.github.com>
2022-10-21 17:56:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// https://teachablemachine.withgoogle.com/models/S7dDWCbf0/
+// https://teachablemachine.withgoogle.com/models/fwZ3Gi2RJ/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+})
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+
+classifier=ml5.imageClassifier(" https://teachablemachine.withgoogle.com/models/S7dDWCbf/model.jsol",modelLoaded);
+function modelLoaded(){
+    console.log("modelLoaded")
+}
